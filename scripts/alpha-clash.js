2b56bf3143c3f79cf6e395b516ef1403d287195b
Mushfiqur0R <musfikur.dev@gmail.com>
2024-02-15 14:55:32 +0600

    Update Score And Life Based On Right Or Wrong Key Press

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,9 +10,9 @@
 // }
 
 
-function handleKeyboardKeyUpEvent(event){
+function handleKeyboardKeyUpEvent(event) {
     const playerPressed = event.key;
-    console.log('player pressed',playerPressed);
+    console.log('player pressed', playerPressed);
 
     // get the expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -21,20 +21,45 @@ function handleKeyboardKeyUpEvent(event){
     console.log(playerPressed, expectedAlphabet);
 
     //check matched or not
-    if(playerPressed === expectedAlphabet){
+    if (playerPressed === expectedAlphabet) {
         console.log('you get a point');
-        console.log('you have pressed correctly', expectedAlphabet);
+        // update score
+        // 1. get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        console.log(currentScore);
+
+        // 2. increase the score by 1
+        const newScore = currentScore + 1;
+        
+        // 3. show the updated score
+        currentScoreElement.innerText = newScore;
+
+
+        // console.log('you have pressed correctly', expectedAlphabet);
         removeBackgroundColorById(expectedAlphabet);
         continueGame();
-    }else{
+    } else {
         console.log('you missed. you lost a life');
+
+        // step1: get the current life number
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        // step2: reduce the life count
+        const newLife = currentLife - 1;
+
+        // step3: display the updated life count
+        currentLifeElement.innerText = newLife;
     }
 }
 // captured keyboard key press
-document.addEventListener('keyup',handleKeyboardKeyUpEvent );
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
 
 
-function continueGame(){
+function continueGame() {
     //step 1: generate a random alphabet
     const alphabet = getRandomAlphabet();
     // console.log('your random alphabet', alphabet);
@@ -45,12 +70,12 @@ function continueGame(){
 
     //set background color
     setBackgroundColorById(alphabet);
-    
+
 }
 
 
 
-function play(){
+function play() {
     hideElementById('home-screen');
     showElementById('play-ground');
     continueGame();

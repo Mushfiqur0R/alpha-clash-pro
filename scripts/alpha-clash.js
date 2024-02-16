004074c7f7240c2d014198ed507f859cc51d1d71
Mushfiqur0R <musfikur.dev@gmail.com>
2024-02-16 14:57:13 +0600

    Implement Play Again And Reset Life And Score

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -23,36 +23,54 @@ function handleKeyboardKeyUpEvent(event) {
     //check matched or not
     if (playerPressed === expectedAlphabet) {
         console.log('you get a point');
+
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+
+
+
+        // ----------------------------------------
         // update score
         // 1. get the current score
-        const currentScoreElement = document.getElementById('current-score');
-        const currentScoreText = currentScoreElement.innerText;
-        const currentScore = parseInt(currentScoreText);
-        console.log(currentScore);
-
-        // 2. increase the score by 1
-        const newScore = currentScore + 1;
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // console.log(currentScore);
         
-        // 3. show the updated score
-        currentScoreElement.innerText = newScore;
 
+        // // 2. increase the score by 1
+        // const newScore = currentScore + 1;
+        
+        // // 3. show the updated score
+        // currentScoreElement.innerText = newScore;
 
+        // start new round
         // console.log('you have pressed correctly', expectedAlphabet);
         removeBackgroundColorById(expectedAlphabet);
         continueGame();
     } else {
         console.log('you missed. you lost a life');
 
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
+
+        if(updatedLife === 0){
+            gameOver();
+        }
+
+        //----------------------------------------------
         // step1: get the current life number
-        const currentLifeElement = document.getElementById('current-life');
-        const currentLifeText = currentLifeElement.innerText;
-        const currentLife = parseInt(currentLifeText);
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
 
-        // step2: reduce the life count
-        const newLife = currentLife - 1;
+        // // step2: reduce the life count
+        // const newLife = currentLife - 1;
 
-        // step3: display the updated life count
-        currentLifeElement.innerText = newLife;
+        // // step3: display the updated life count
+        // currentLifeElement.innerText = newLife;
     }
 }
 // captured keyboard key press
@@ -76,7 +94,18 @@ function continueGame() {
 
 
 function play() {
+    // hide everything show only the playground
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
     continueGame();
+
+    // reset score and life
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
 }
